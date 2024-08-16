3419e7c6346e98e0061ec64d745232ecc30ec1fc
Daffa Reyhan <daffa@sinergi.co.id>
2024-08-16 11:20:00 +0700

    #update

--- js/app.js
+++ js/app.js
@@ -26,65 +26,70 @@ canvas.add(img).setActiveObject(img).renderAll();
 reader.readAsDataURL(e.target.files[0]);
 }
 
-fabric.Image.fromURL('img/DIRGAHAYU (2).png', function(img) {
-    img.set({
-      scaleX: canvas.width / img.width,
-      scaleY: canvas.height / img.height,
-      originX: "left",
-      originY: "top",
-      selectable: false, // Agar overlay tidak dapat dipilih atau dipindahkan
-      evented: false, // Agar overlay tidak dapat merespon event seperti klik
-    });
-canvas.setOverlayImage(img, canvas.renderAll.bind(canvas));
+fabric.Image.fromURL("img/DIRGAHAYU.png", function (img) {
+  img.set({
+    scaleX: canvas.width / img.width,
+    scaleY: canvas.height / img.height,
+    originX: "left",
+    originY: "top",
+    selectable: false, // Agar overlay tidak dapat dipilih atau dipindahkan
+    evented: false, // Agar overlay tidak dapat merespon event seperti klik
+  });
+  canvas.setOverlayImage(img, canvas.renderAll.bind(canvas));
 });
-	
+
 canvas.on({
-'object:moving': function(e) {
-e.target.opacity = 0.6;
-},
-'object:modified': function(e) {
-e.target.opacity = 1;
-}
+  "object:moving": function (e) {
+    e.target.opacity = 0.6;
+  },
+  "object:modified": function (e) {
+    e.target.opacity = 1;
+  },
 });
-	
+
 function selectFile() {
   document.getElementById("uploader").click();
 }
-	
-$("#save").click(function(){
-$("#c").get(0).toBlob(function(blob){
-saveAs(blob, "image.png");
-});
+
+$("#save").click(function () {
+  $("#c")
+    .get(0)
+    .toBlob(function (blob) {
+      saveAs(blob, "image.png");
+    });
 });
 
 // share
 document.getElementById("shared").addEventListener("click", () => {
-alert("Bagikan Twibbon ini ke media sosial Anda?");
+  alert("Bagikan Twibbon ini ke media sosial Anda?");
 
-const dataUrl = c.toDataURL(); 
-fetch(dataUrl)
-.then(res => res.blob())
-.then(blob => {
-//console.log(blob)
-const filesArray = [new File([blob], 'image.png', { type: blob.type, lastModified: new Date().getTime() })];
-console.log(filesArray);
-const shareData = {
-title: "FREE AND SIMPLE PFP GENERATOR",
-text: "FREE AND SIMPLE PFP GENERATOR",
-url: "https://badut.pages.dev/",
-files: filesArray
-};
-console.log(shareData);
-if (navigator.share) {
-navigator.share(
-shareData
-)
-.then(() => alert("Terima kasih telah berbagi.")) 
-.catch(error => alert("Bagikan dibatalkan!", error)); 
-} else {
-alert('navigator.share not available'); 
-}
-})
+  const dataUrl = c.toDataURL();
+  fetch(dataUrl)
+    .then((res) => res.blob())
+    .then((blob) => {
+      //console.log(blob)
+      const filesArray = [
+        new File([blob], "image.png", {
+          type: blob.type,
+          lastModified: new Date().getTime(),
+        }),
+      ];
+      console.log(filesArray);
+      const shareData = {
+        title: "FREE AND SIMPLE PFP GENERATOR",
+        text: "FREE AND SIMPLE PFP GENERATOR",
+        files: filesArray,
+      };
+      console.log(shareData);
+      if (navigator.share) {
+        navigator
+          .share(shareData)
+          .then(() => alert("Terima kasih telah berbagi."))
+          .catch((error) => alert("Bagikan dibatalkan!", error));
+      } else {
+        alert("navigator.share not available");
+      }
+    });
 });
 
 // hidden button
